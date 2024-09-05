e680359fdaa1cad7df140c1a72687f5cd924eefe
ThaisVilasboas <144953802+ThaisVilasboas@users.noreply.github.com>
2024-09-04 22:17:04 -0300

    Add files via upload

---
+++ ProgOrientadaOBJ/ProgramaçãoOrientada001/ProgramaçãoOrientada001/Pessoa.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaçãoOrientada001
+{
+    internal class Pessoa
+    {
+        String nome;
+        int idade;
+        string sexo;
+
+        public Pessoa() 
+        {
+            nome = " Nome ";
+            idade =  0;
+            sexo = " M/F ";
+        }
+
+        public void setNome(string n)
+        {
+            nome = n;
+        }
+        public string getNome() 
+        { 
+            return nome;
+        }
+        public void setIdade(int idade) 
+        
+        {
+             this.idade = idade;
+
+        }
+        public int getIdade() 
+        { 
+             return idade;
+        }
+        public void setSexo(string sexo)
+        
+        { 
+            this.sexo = sexo;
+        }
+        public string getSexo() 
+      
+        { 
+            return sexo;
+
+        }
+
+        public override string ToString() 
+        
+        { 
+
+            return " \nNome:  " +nome+ "\nIdade: " +idade+   " \nSexo: " + sexo +" \n ";
+
+        }
+        
+    }
+
+
+}
---
+++ ProgOrientadaOBJ/ProgramaçãoOrientada001/ProgramaçãoOrientada001/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaçãoOrientada001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine(pessoa.ToString());
+
+            Console.Write(" Informe o nome da pessoa: ");
+            pessoa.setNome(Console.ReadLine());
+
+            Console.Write(" Informe o sexo da pessoa (M/F): ");
+            string aux = Console.ReadLine();
+            pessoa.setSexo(aux);
+
+            Console.Write(" Informe a idade da pessoa: ");
+            pessoa.setIdade(int.Parse(Console.ReadLine()));
+
+            //Console.WriteLine($" \n O nome da pessoa é : {pessoa.getNome()}");
+            //Console.WriteLine($" A idade da pessoa é : {pessoa.getIdade()}"); 
+            //Console.WriteLine($" O sexo da pessoa é : {pessoa.getSexo()}");
+
+            Console.Write(pessoa.ToString());
+
+            Pessoa pessoa1;
+            pessoa1 = new Pessoa();
+
+
+            Console.ReadKey();
+        }            
+            
+    }
+}
