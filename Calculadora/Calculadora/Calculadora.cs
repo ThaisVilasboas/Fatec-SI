b1fab1c9d7b978caa93ecd7706044751ef239a3a
ThaisVilasboas <144953802+ThaisVilasboas@users.noreply.github.com>
2024-09-20 21:29:16 -0300

    Add files via upload

---
+++ Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Calculadora
+
+
+    {
+
+        public void Somar(double x, double y)
+
+        {
+
+            Console.WriteLine($" {x} + {y} = {x + y} ");
+
+        }
+
+        public void Subtrair(double x, double y)
+
+        {
+
+            Console.WriteLine($" {x} - {y} = {x - y} ");
+
+        }
+
+        public void Dividir(double x, double y)
+
+        {
+
+            Console.WriteLine($" {x} / {y} = {x / y} ");
+
+        }
+        public void Multiplicar(double x, double y)
+
+        {
+
+            Console.WriteLine($" {x} * {y} = {x * y} ");
+
+        }
+
+
+
+    }
+}
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" Calculadora ");
+            Console.WriteLine();
+
+            Calculadora calculo = new Calculadora();
+            calculo.Somar(4.5, 5.2);
+            calculo.Subtrair(6 , 7);
+
+
+            Console.ReadKey();
+        }
+    }
+}
